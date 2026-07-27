4b611ec127495c844bb43f8f9783eea01fd58d86
yuramashchak <yuramashchak@gmail.com>
2026-07-27 02:28:25 +0200

    my first hm react

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/02-react-cafe/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
